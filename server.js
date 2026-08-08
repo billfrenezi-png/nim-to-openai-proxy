220fca952f39dae4f9efdf931e71d1a7a0fcf783
billfrenezi-png <billfrenezi@gmail.com>
2026-08-07 22:30:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const MODEL_MAPPING = {
   'gpt-3.5o': 'nvidia/nemotron-mini-4b-instruct',
   'gpt-4-flash': 'deepseek-ai/deepseek-v4-flash',
   'glm-5.2': 'z-ai/glm-5.2',
-  'mistral': 'mistralai/mistral-large-3-675b-instruct-2512',
+  'mistral': 'thinkingmachines/inkling',
   'mistral-turbo': 'mistralai/mistral-medium-3.5-128b',
   'mistral-pro': 'mistralai/mistral-small-4-119b-2603',
   'mistral-nemo': 'mistralai/mistral-nemotron',
